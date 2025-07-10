51dbe81ceb9c7ef638233d7232bf090c351131dc
billwu90325 <billwu90325@gmail.com>
2025-07-10 10:58:10 +0800

    #191

--- Blind75/191. Number of 1 Bits.cpp
+++ Blind75/191. Number of 1 Bits.cpp
@@ -2,10 +2,9 @@ class Solution {
     public:
         int hammingWeight(int n) {
             int sum = 0;
-            for (int i = 31; i >= 0; i--) {
-                long exp = pow(2, i);
-                sum += n / exp;
-                n = n % exp;
+            while (n > 0) {
+                sum += n % 2;
+                n /= 2;
             }
             return sum;
         }
