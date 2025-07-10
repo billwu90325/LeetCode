11fddef4fed98bb06013565af71f2858deae74c7
billwu90325 <billwu90325@gmail.com>
2025-07-10 16:44:29 +0800

    #125

--- Blind75/125. Valid Palindrome.cpp
+++ Blind75/125. Valid Palindrome.cpp
@@ -1,24 +1,39 @@
 class Solution {
     public:
         bool isPalindrome(string s) {
-    
-            vector<char> text;
-            for (int i = 0; i < s.length(); i++) {
-                if ((s[i] <= 'Z' && s[i] >= 'A') || 
-                    (s[i] <= 'z' && s[i] >= 'a') ||
-                    (s[i] >= '0' && s[i] <= '9')) {
-                    text.push_back(tolower(s[i]));
-                }
-            }
             
+            // Improved SOL: save space complexity, check the original array directly
             int left = 0;
-            int right = text.size() - 1;
+            int right = s.size() -1;
+    
             while (left < right) {
-                if (text[left] == text[right]) {
-                    left ++;
-                    right --;
-                } else return false;
+                while (left < right && !isalnum(s[left])) left++;
+                while (left < right && !isalnum(s[right])) right--;
+    
+                if (tolower(s[left]) != tolower(s[right])) return false;
+                left ++;
+                right --;
             }
+    
             return true;
+    
+            // vector<char> text;
+            // for (int i = 0; i < s.length(); i++) {
+            //     if ((s[i] <= 'Z' && s[i] >= 'A') || 
+            //         (s[i] <= 'z' && s[i] >= 'a') ||
+            //         (s[i] >= '0' && s[i] <= '9')) {
+            //         text.push_back(tolower(s[i]));
+            //     }
+            // }
+    
+            // int left = 0;
+            // int right = text.size() - 1;
+            // while (left < right) {
+            //     if (text[left] == text[right]) {
+            //         left ++;
+            //         right --;
+            //     } else return false;
+            // }
+            // return true;
         }
     };
